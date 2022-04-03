932194de984a67bbe693c1ddc2e69179d013f33d
silivalleydev <silivalleydev@gmail.com>
2022-04-03 17:12:46 +0900

    dfkd

--- src/fileUploadFunction.js
+++ src/fileUploadFunction.js
@@ -6,18 +6,21 @@ export function onFileUpload(e) {
     const REGION = "버킷리전";
     const S3_BUCKET = '버킷이름';
 
+    // AWS ACCESS KEY를 세팅합니다.
     AWS.config.update({
       accessKeyId: ACCESS_KEY,
       secretAccessKey: SECRET_ACCESS_KEY
     });
 
+    // 버킷에 맞는 이름과 리전을 설정합니다.
     const myBucket = new AWS.S3({
       params: { Bucket: S3_BUCKET},
       region: REGION,
     });
+
     const file = e.target.files[0];
-    console.log(file);
-    console.log(file.name);
+
+    // 파일과 파일이름을 넘겨주면 됩니다.
     const params = {
       ACL: 'public-read',
       Body: file,
